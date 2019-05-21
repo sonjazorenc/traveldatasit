765e4a15e4a04fc50d19d287a74e3f693fe9e743
sonjazorenc <50211142+sonjazorenc@users.noreply.github.com>
2019-05-21 11:30:36 +0200

    Update and rename eventtraveldata.cpp to eventtraveldataexample.cpp

---
+++ eventtraveldataexample.cpp
@@ -0,0 +1,17 @@
+// Example of a event sent by the server to the client 
+// Message is the travel data
+
+#ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
+#include <csignal>
+#endif
+#include <chrono>
+#include <condition_variable>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include <vsomeip/vsomeip.hpp>
+
+#include "sample-ids.hpp"
+
