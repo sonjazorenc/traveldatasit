e926d0cd8ca6bfde12ceb566685b85d9e638b586
sonjazorenc <50211142+sonjazorenc@users.noreply.github.com>
2019-05-21 12:07:06 +0200

    Update subscribetraveldataexample.cpp

--- subscribetraveldataexample.cpp
+++ subscribetraveldataexample.cpp
@@ -1,1 +1,39 @@
+//the subscription by the client which enables the server to send
+//the client in specific time intervals the latest travel data
 
+
+#ifndef VSOMEIP_ENABLE_SIGNAL_HANDLING
+#include <csignal>
+#endif
+#include <chrono>
+#include <condition_variable>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+
+#include <vsomeip/vsomeip.hpp>
+
+#include "traveldataids.hpp"
+
+class client_sample {
+  public:
+    client_sample(bool _use_tcp) :
+            app_(vsomeip::runtime::get()->create_application()), use_tcp_(
+                    _use_tcp) {
+    }
+
+   bool init() {
+        if (!app_->init()) {
+            std::cerr << "Not possible to send travel data because no application initialized currently" << std::endl;
+            return false;
+        }
+     
+      std::cout << "Client settings [protocol="
+                << (use_tcp_ ? "TCP" : "UDP")
+                << "]"
+                << std::endl;
+     
+     
+  
+  
