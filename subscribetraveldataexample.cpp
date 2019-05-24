fde8e2cdcd5b968f590c6781f271aff836a52757
sonjazorenc <50211142+sonjazorenc@users.noreply.github.com>
2019-05-24 10:32:56 +0200

    Update subscribetraveldataexample.cpp

--- subscribetraveldataexample.cpp
+++ subscribetraveldataexample.cpp
@@ -120,17 +120,9 @@ if the engine has already started the driver setting procol appears on the conso
     void on_message(const std::shared_ptr<vsomeip::message> &_response) {
                std::stringstream its_message;
         its_message << "Received a notification for Event ["
-                << std::setw(4)    << std::setfill('0') << std::hex
-                << _response->get_service() << "."
-                << std::setw(4) << std::setfill('0') << std::hex
-                << _response->get_instance() << "."
-                << std::setw(4) << std::setfill('0') << std::hex
-                << _response->get_method() << "] to Client/Session ["
-                << std::setw(4) << std::setfill('0') << std::hex
-                << _response->get_client() << "/"
-                << std::setw(4) << std::setfill('0') << std::hex
-                << _response->get_session()
-                << "] = ";
+                    << "/n"
+                    << " Getting Travel Data]"
+                    <<std::endl;
         std::shared_ptr<vsomeip::payload> its_payload =
                 _response->get_payload();
         its_message << "(" << std::dec << its_payload->get_length() << ") ";
